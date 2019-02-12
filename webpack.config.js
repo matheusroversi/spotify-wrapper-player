7e3e7135abda403d7433247a15c0c6b9d8c45a35
Matheus Roversi <matheusroversi@gmail.com>
2019-02-12 02:07:00 -0200

    configuration travis

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, "./example"),
+    path: path.resolve(__dirname, "./dist"),
   },
   devServer: {
     contentBase: path.resolve(__dirname,'./example'),
